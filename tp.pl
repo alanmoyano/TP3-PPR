66d8fd04daeef4c78a2df3595c9391531c241b7a
Alan Moyano <alanmoyano203@gmail.com>
2023-11-04 22:33:08 -0300

    Todo menos el 5

--- tp.pl
+++ tp.pl
@@ -15,7 +15,7 @@ equipo(1, avengers, 0.78).
 equipo(2, asgard, 0.67).
 equipo(3, wakanda, 0.74).
 equipo(4, xmen, 0.91).
-equipo(5, mercenarios).
+equipo(5, mercenarios, 0.90).
 
 % poderes/3:
 poder(tec,'Tecnología',	10).
@@ -54,17 +54,49 @@ enemigo_de(Personaje1, Personaje2) :-
 %       nombre real, nombre del equipo al que pertenece y
 %       lista de poderes (deberá mostrar los nombres de los poderes no lo códigos). Regla sugerida: regla1/4.
 
+regla1(Codigo, NombreReal, NombreEquipo, Poderes) :-
+    personaje(Codigo, _, NombreReal, CodigoEquipo, ListaPoderes),
+    equipo(CodigoEquipo, NombreEquipo, _),
+    findall(Poder, (member(X, ListaPoderes), poder(X, Poder, _)), Poderes).
+
 % 2)	Si existe o no algún personaje que siendo de avengers tenga 
 %       como poder la fuerza sobrehumana pero no cuenta con agilidad. Regla sugerida: regla2/0.
 
+regla2() :-
+    personaje(_,_,_,1,ListaPoderes),
+    member(fs, ListaPoderes),
+    not(member(ag, ListaPoderes)), !.
+
 % 3)	Para cierto código de personaje, calcular su peso total 
 %       en base a unidades totales de combate según sus poderes. Regla sugerida: regla3/2. 
-%       Este valor se calcula de la siguiente manera: Se suma el peso en unidades de combate de cada poder 
-        % y se lo multiplica por un factor que depende del equipo al que pertenece el personaje.
+%       Este valor se calcula de la siguiente manera:
+%       Se suma el peso en unidades de combate de cada poder 
+%       y se lo multiplica por un factor que depende del equipo
+%       al que pertenece el personaje.
 
-% 4)	Determinar para un nombre de equipo, la lista de nombres de personajes que lo conforman. Regla sugerida: regla4/3.
+regla3(Codigo, Peso) :-
+    personaje(Codigo, _, _, CodigoEquipo, ListaPoderes),
+    equipo(CodigoEquipo, _, FactorEquipo),
+    findall(Peso, (member(Poder, ListaPoderes), poder(Poder, _, Peso)), PesosPoderes),
+    sumlist(PesosPoderes, SumaPesosPoderes),
+    Peso is SumaPesosPoderes * FactorEquipo.
 
-% 5)	Dado el código de un equipo generar una lista de tuplas. Cada tupla deberá tener 2 elementos.
-%       El primer elemento de cada tupla es el nombre de un personaje del equipo y el segundo elemento es
-%       el peso total en base a unidades totales de combate según los poderes del personaje
+% 4)	Determinar para un nombre de equipo, la lista de nombres de personajes 
+%       que lo conforman. Regla sugerida: regla4/3.
+
+regla4(NombreEquipo, ListaNombres) :-
+    equipo(CodigoEquipo, NombreEquipo, _),
+    findall(NombrePersonaje, (
+            personaje(_, _, NombrePersonaje, CodigoEquipo, _)
+        ), ListaNombres).
+
+% 5)	Dado el código de un equipo generar una lista de tuplas. 
+%       Cada tupla deberá tener 2 elementos.
+%       El primer elemento de cada tupla es el nombre de un personaje 
+%       del equipo y el segundo elemento es
+%       el peso total en base a unidades totales de combate según
+%       los poderes del personaje
 %       (se puede reutilizar la regla 3): regla5/1.
+
+regla5() :-
+    
